aaf5efa0ca8b2cf48a624aaa6b2cd1123813b4b2
Alex Milanes <milanesalex941@gmail.com>
2026-04-06 12:15:50 -0400

    Create HVLCS algorithm

---
+++ src/hvlcs.py
@@ -0,0 +1,24 @@
+from parse import read_input
+
+def hvlcs(character_map, A, B) -> list[list[int]]:
+    # create the 2D dp table of size len(A) + 1 by len(B) + 1, initialized to 0
+    dp = [[0 for i in range(len(B) + 1)] for j in range(len(A) + 1)]
+
+    for i in range(1, len(A) + 1):
+        for j in range(1, len(B) + 1):
+            # if the two characters match, add the value of the character to the dp value of the previous characters
+            if A[i - 1] == B[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + character_map[A[i - 1]]
+            # take the maximum of the previous characters and the current value
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], dp[i][j])
+
+    return dp
+
+
+if __name__ == "__main__":
+    _, character_map, A, B = read_input("file1")
+    
+    # get the dp table
+    dp = hvlcs(character_map, A, B)
+
+    print(f"{dp[len(A)][len(B)]}")
