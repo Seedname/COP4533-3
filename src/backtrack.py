e1cee5e8c46bd1658ca6832654dbaf1ed4085c58
Alex Milanes <milanesalex941@gmail.com>
2026-04-06 17:06:51 -0400

    Fix backtracking algorithm

--- src/backtrack.py
+++ src/backtrack.py
@@ -1,22 +1,21 @@
 from parse import read_input
 from hvlcs import hvlcs
 
-
-def backtrack(dp, A) -> str:
+def backtrack(dp, A, B, character_map) -> str:
     i = len(dp) - 1
     j = len(dp[0]) - 1
     res = []
 
     while i > 0 and j > 0:
-        # if the value is equal to the previous value, then this character must not be used
-        if dp[i][j] == dp[i - 1][j]:
-            i -= 1
-        elif dp[i][j] == dp[i][j - 1]:
-            j -= 1
-        # otherwise, we use the character and add it to the result string
-        else:
+        # if the characters are equal and the values match, we used the character and should add it to the result string
+        if A[i - 1] == B[j - 1] and dp[i][j] == dp[i - 1][j - 1] + character_map[A[i - 1]]:
             res.append(A[i - 1])
             i -= 1
+            j -= 1
+        # otherwise, we check which previous value is optimal and move accordingly
+        elif dp[i - 1][j] >= dp[i][j - 1]:
+            i -= 1
+        else:
             j -= 1
 
     return ''.join(reversed(res))
@@ -29,6 +28,6 @@ if __name__ == "__main__":
     dp = hvlcs(character_map, A, B)
 
     # get the hvlcs string
-    res = backtrack(dp, A)
+    res = backtrack(dp, A, B, character_map)
 
     print(res)
